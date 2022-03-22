2aaf6b5bd3547e4594a5f4d47f842ff02c74ff9d
zhenghui <zsh6147@163.com>
2022-03-22 23:07:38 +0800

    simplify the code logic

--- change-ibus-input-method.sh
+++ change-ibus-input-method.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-
 # this is a script that change the ibus input method.
-# only tested under jjj
+# only tested under bash version 5.1.12
+
 input_methods=("pinyin" "xkb:us::eng")
-
 current_input_method=$(ibus engine)
-
-declare input_methods_to_set
-next_index=-1
+declare input_method_to_set
+index=-1
 
 for item in ${input_methods[@]};
 do
-	next_index=$(($next_index+1))
+	index=$(($index+1))
 	if [ ${item} == ${current_input_method} ]; then
-		next_index=$(($next_index+1))
+		next_index=$(($index+1))
+		if [ $next_index == ${#input_methods[@]} ]; then
+			next_index=0
+		fi
+		input_method_to_set=${input_methods[$next_index]}
 		break
 	fi
 done
-# the next index is reach the end of array items, so we need to reset it to zero.
-if [ $next_index != -1 ] && [ $next_index == ${#input_methods[@]} ]; then
-	next_index=0
-fi
-
-if [ $next_index == -1 ]; then
-	echo 'error can not find the next input method to set, the current input method is '$current_input_method
+if [ $input_method_to_set == "" ]; then
+	echo "can not find input method to set, current input method is "$current_input_method
 else
-	ibus engine ${input_methods[$next_index]}
-	echo "have set the input method of ibus to ${input_methods[$next_index]}"
+	ibus engine ${input_method_to_set}
+	echo "have set the input method of ibus to ${input_method_to_set}"
 fi
